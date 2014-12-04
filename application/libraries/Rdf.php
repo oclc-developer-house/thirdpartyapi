3cce12877f1771f8e5e9b46f351723eba76946bb
Bilal Khalid <bilal.khalid@utoronto.ca>
2014-12-04 11:38:12 -0500

    incorporated JsonLd for the Rdf library

--- application/libraries/Rdf.php
+++ application/libraries/Rdf.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once APPPATH . "third_party/easyrdf/lib/EasyRdf.php";
+require_once APPPATH . "third_party/easyrdf/lib/EasyRdf/Serialiser/JsonLd.php";
+require_once FCPATH . "vendor/ml/json-ld/ML/JsonLD/JsonLD.php";
 
 class Rdf {
 	
